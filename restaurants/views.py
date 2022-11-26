b6475c1661c32690702f64b07279377e38833ed7
Weorano <ignatov.roman.a@mail.ru>
2022-11-26 14:30:57 +0300

    add FoodList view in restaurants

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.db.models import Count, Case, When, IntegerField, Prefetch
+from rest_framework import generics
 
-# Create your views here.
+from restaurants.models import FoodCategory, Food
+from restaurants.serializers import FoodListSerializer
+
+
+class FoodList(generics.ListAPIView):
+    """
+    Просмотр списка всех блюд для раздела меню в системе.
+
+    """
+
+    queryset = FoodCategory.objects.annotate(
+        num_published=Count(Case(
+            When(food__is_publish=True, then=1),
+            output_field=IntegerField(),
+        ))
+    ).filter(num_published__gt=0).prefetch_related(
+        Prefetch("food", queryset=Food.objects.filter(is_publish=True))
+    )
+
+    serializer_class = FoodListSerializer
